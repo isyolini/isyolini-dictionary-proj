1e5cab5663dbdc7c15b599fc5864e90dd9dcf507
isyolini <50636149+isyolini@users.noreply.github.com>
2023-08-30 19:53:13 +0200

    Added axios and SheCodes API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching ${keyword}`);
+
+    const apiKey = "ate4fc772f93a185a4d70db0f2foe64c";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
